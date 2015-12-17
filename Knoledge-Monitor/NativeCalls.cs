5c9ce92d71f0574569f095c55b57b2dfda693028
petebrahnt <pete@calliste-it.co.uk>
2015-12-17 20:51:36 +0000

    copy from prototype project

---
+++ Knoledge-Monitor/NativeCalls.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knoledge_Monitor
+{
+    public static class NativeCalls
+    {
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int description, int reservedValue);
+    }
+}
